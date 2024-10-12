10913a337e9df6e9a837f80579aef0ac1d9f2725
minhbn3107 <109571044+minhbn3107@users.noreply.github.com>
2024-10-12 12:44:51 +0700

    Delete lab2 directory

--- lab2/forEach.js
+++
@@ -1,8 +0,0 @@
-Array.prototype.myForEach = function (callback) {
-    for (let i = 0; i < this.length; i++) {
-        callback(this[i], i, this);
-    }
-};
-
-const numbers = [1, 2, 3, 4];
-numbers.myForEach((num, index) => console.log(`Index ${index}: ${num}`));
